f6d16c91c41ef94bd64df733e0570c9ec3e36e98
Matt Thomas <matt@betweenbrain.com>
2012-11-08 15:37:46 -0500

    Code formatting. Fix CSS bug.

--- code/gnomeinterface.php
+++ code/gnomeinterface.php
@@ -36,26 +36,26 @@ class plgSystemGnomeinterface extends JPlugin
             // Change Article Edit Layout
             if ($option == 'com_content' && $view == 'article' && $layout == 'edit' && $this->params->get('content_editlayout', '1') == true) {
                 $doc->addScriptDeclaration("
-				window.addEvent('domready', function() {
-					var panel = new Element('div', {
-						'class'	: 'panel'});
-					panel.set('html', '<h3 id=\"article-details\" class=\"pane-toggler title\"><a href=\"javascript:void(0);\"><span>" . JText::_('PLG_SYSTEM_GNOMEINTERFACE_ARTICLE_DETAILS') . "</span></a></h3><div class=\"pane-slider content\"><fieldset class=\"panelform\" id=\"article-details-content\"></fieldset></div>');
+                window.addEvent('domready', function() {
+                    var panel = new Element('div', {
+                        'class'    : 'panel'});
+                    panel.set('html', '<h3 id=\"article-details\" class=\"pane-toggler title\"><a href=\"javascript:void(0);\"><span>" . JText::_('PLG_SYSTEM_GNOMEINTERFACE_ARTICLE_DETAILS') . "</span></a></h3><div class=\"pane-slider content\"><fieldset class=\"panelform\" id=\"article-details-content\"></fieldset></div>');
 
-					if ($('content-sliders-1') == null) { 
-						$('content-sliders-').grab(panel, 'top');
-					} else {
-						$('content-sliders-1').grab(panel, 'top');
-					}
-					
-					var index = 0;
-					$$('ul.adminformlist').each(function(element) {
-						if (index == 0) {
-							$('article-details-content').grab(element);
-						}
-						index++;
-					});
-				});
-				");
+                    if ($('content-sliders-1') == null) { 
+                        $('content-sliders-').grab(panel, 'top');
+                    } else {
+                        $('content-sliders-1').grab(panel, 'top');
+                    }
+                    
+                    var index = 0;
+                    $$('ul.adminformlist').each(function(element) {
+                        if (index == 0) {
+                            $('article-details-content').grab(element);
+                        }
+                        index++;
+                    });
+                });
+                ");
 
                 $doc->addStyleDeclaration("
                     #jform_articletext,
@@ -70,41 +70,42 @@ class plgSystemGnomeinterface extends JPlugin
                     }
                     #content-box #toolbar-box {
                         margin-bottom: 0;
-                    }");
+                    }
+                    ");
             }
 
             if ($option == 'com_modules' && $view == 'select') {
                 $doc->addStyleDeclaration("
-				ul#new-modules-list, ul#new-modules-list2 {
-					border - top: 1px solid #666666;
-					list-style: none outside none;
-					margin: 0;
-					float: left;
-					padding: 5px 0 0 15px;
-					width: 45 %;
-				}
-				ul#new-modules-list li, ul#new-modules-list2 li {
-					display: block;
-					float: none;
-					list-style: none outside none;
-					margin: 0 20px 0 0;
-					width: 100 %;
-				}
-				ul#new-modules-list li a, ul#new-modules-list2 li a {
-					font - size: 1.091em;
-					line - height: 1.8em;
-				}
-ul #new-modules-list2 {
-float: right;
-}
-				");
+                ul#new-modules-list, ul#new-modules-list2 {
+                    border-top: 1px solid #666666;
+                    list-style: none outside none;
+                    margin: 0;
+                    float: left;
+                    padding: 5px 0 0 15px;
+                    width: 45 %;
+                }
+                ul#new-modules-list li, ul#new-modules-list2 li {
+                    display: block;
+                    float: none;
+                    list-style: none outside none;
+                    margin: 0 20px 0 0;
+                    width: 100 %;
+                }
+                ul#new-modules-list li a, ul#new-modules-list2 li a {
+                    font-size: 1.091em;
+                    line-height: 1.8em;
+                }
+                ul #new-modules-list2 {
+                float: right;
+                }
+                ");
                 $doc->addScriptDeclaration("
-				window . addEvent('domready', function () {
+                window . addEvent('domready', function () {
                     var
                     index = 1;
                     var right = new Element('ul', {'id':'new-modules-list2'});
-					var midpoint = Math . round($$('#new-modules-list li') . length / 2);
-					$$('#new-modules-list li') . each(function (element) {
+                    var midpoint = Math . round($$('#new-modules-list li') . length / 2);
+                    $$('#new-modules-list li') . each(function (element) {
                         if (index > midpoint) {
                             element . inject(right);
                         } else {
@@ -112,9 +113,9 @@ float: right;
                         }
                         index++;
                     });
-					right . inject('new-modules-list', 'after');
-				});
-				");
+                    right . inject('new-modules-list', 'after');
+                });
+                ");
             }
 
             if ($option == 'com_modules' && $view == 'module' && $layout == 'edit') {
@@ -136,31 +137,31 @@ float: right;
                 sort($positions);
 
                 $script = "
-				var position_default;
-				window.addEvent('domready', function() {
-					position_default = $('jform_position').get('value');
-					var button = $('jform_position-lbl').getNext('div.button2-left').getFirst('div').getFirst('a.modal');
-					var link = button.get('href');
-					button.set('href', link+'" . $filters . "');
-					var list = new Element('select', {'id':'position_list'});
-					list.adopt(new Element('option', {'value':'','text':'" . JText::_('PLG_SYSTEM_GNOMEINTERFACE_SELECT') . "'}));
-					";
+                var position_default;
+                window.addEvent('domready', function() {
+                    position_default = $('jform_position').get('value');
+                    var button = $('jform_position-lbl').getNext('div.button2-left').getFirst('div').getFirst('a.modal');
+                    var link = button.get('href');
+                    button.set('href', link+'" . $filters . "');
+                    var list = new Element('select', {'id':'position_list'});
+                    list.adopt(new Element('option', {'value':'','text':'" . JText::_('PLG_SYSTEM_GNOMEINTERFACE_SELECT') . "'}));
+                    ";
 
                 foreach ($positions as $position) {
                     $script .= "list.adopt(new Element('option', {'value':'" . $position . "','text':'" . $position . "'}));" . PHP_EOL;
                 }
 
                 $script .= "
-					list.injectAfter('jform_position');
-					$('position_list').addEvent('change', function() {
-						if (this.get('value') != '') {
-							$('jform_position').set('value', this.get('value'));
-						} else {
-							$('jform_position').set('value', position_default);
-						}
-					});
-				});
-				";
+                    list.injectAfter('jform_position');
+                    $('position_list').addEvent('change', function() {
+                        if (this.get('value') != '') {
+                            $('jform_position').set('value', this.get('value'));
+                        } else {
+                            $('jform_position').set('value', position_default);
+                        }
+                    });
+                });
+                ";
 
                 $doc->addScriptDeclaration($script);
             }
